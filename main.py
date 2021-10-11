e9e4c36085786a231e379da128e9c0a719a712aa
MM-Team-DL <75640712+MM-Team-DL@users.noreply.github.com>
2021-10-11 12:08:34 +0200

    DataLoader added

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from DataPreparation import DataPreparation
+from torch.utils.data import DataLoader
 
 if __name__ == '__main__':
     print("program started")
     directory = 'Datasets/'
     datasets = ['B5', 'B39', 'EM', 'ssTEM', 'TNBC']
     target = 'TNBC'
+    BATCH_SIZE = 128
+
     dataPreparation = DataPreparation(directory, datasets, target)
 
     print("load data")
     images_train, groundtruth_train, images_valid, groundtruth_valid = dataPreparation.load_data()
+
+    train_loader = DataLoader(images_train, batch_size=BATCH_SIZE, shuffle=True)
+    val_loader = DataLoader(groundtruth_train, batch_size=BATCH_SIZE, shuffle=False)
 
     print("load model")
 
